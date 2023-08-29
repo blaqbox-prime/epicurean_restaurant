0e93845c6c7283940d9a8d34b63542a669363577
Karabo Sambo <58365025+blaqbox-prime@users.noreply.github.com>
2023-08-29 11:35:58 +0200

    Delete studio directory

--- studio/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import menu from './menu'
-import product from './product'
-
-export const schemaTypes = [post, author, category, blockContent, menu, product]
